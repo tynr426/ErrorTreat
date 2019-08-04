57062b718952fdfcb4c1324c52365d22d4bb848f
apple <tynr426@126.com>
2019-08-04 22:56:30 +0800

    first commit

---
+++ target/debug/ErrorTreat.d
@@ -0,0 +1,1 @@
+/Users/apple/Documents/rust/ErrorTreat/target/debug/ErrorTreat.dSYM: /Users/apple/Documents/rust/ErrorTreat/src/main.rs
---
+++ target/debug/deps/ErrorTreat-f29453b498a26bc6.d
@@ -0,0 +1,5 @@
+/Users/apple/Documents/rust/ErrorTreat/target/debug/deps/ErrorTreat-f29453b498a26bc6: src/main.rs
+
+/Users/apple/Documents/rust/ErrorTreat/target/debug/deps/ErrorTreat-f29453b498a26bc6.d: src/main.rs
+
+src/main.rs:
